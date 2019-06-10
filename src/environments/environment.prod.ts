d9b1ebd5f0cb16622e224dd10f92683c4ee25cbc
aczuleta <ac.zuleta10@uniandes.edu.co>
2019-06-10 04:07:04 -0500

    dns

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "http://ec2-54-185-153-171.us-west-2.compute.amazonaws.com:8000/graphql"
+  apiUrl: "http://ec2-52-10-114-157.us-west-2.compute.amazonaws.com:8000/graphql"
+  
 };
